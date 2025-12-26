72e1c5e0da0bf37c4341a5c642648ea24dd806ec
M-krizz <mvm.krish82@gmail.com>
2025-12-26 10:12:14 +0530

    feat: Ride Pooling Logic

---
+++ VectraApp/backend/src/modules/pooling/pooling.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PoolingService } from './pooling.service';
+import { PoolGroupEntity } from './pool-group.entity';
+import { RideRequestEntity } from '../ride_requests/ride-request.entity';
+import { TripEntity } from '../trips/trip.entity';
+import { TripRiderEntity } from '../trips/trip-rider.entity';
+import { PoolingManager } from './pooling.manager';
+
+@Module({
+    imports: [
+        TypeOrmModule.forFeature([
+            PoolGroupEntity,
+            RideRequestEntity,
+            TripEntity,
+            TripRiderEntity,
+        ]),
+    ],
+    providers: [PoolingService, PoolingManager],
+    exports: [PoolingService],
+})
+export class PoolingModule { }
