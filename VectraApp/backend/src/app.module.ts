3c7cdbe3c0bccd04e85acb1fbb1d341a66826d34
AkhilD26 <akhilwork611@gmail.com>
2026-02-15 23:48:00 +0530

    chore: fix prettierrc syntax and enforce lf

--- VectraApp/backend/src/app.module.ts
+++ VectraApp/backend/src/app.module.ts
@@ -1,4 +1,4 @@
-// Force CI rebuild (gitattributes fix)
+// Force CI rebuild (strict LF config)
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -69,4 +69,4 @@ dotenv.config();
     },
   ],
 })
-export class AppModule { }
+export class AppModule {}
