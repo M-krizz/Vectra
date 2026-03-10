c0ced181ebea6d8f88a9436f4e1b95f84f77eafb
M-krizz <mvm.krish82@gmail.com>
2026-03-11 01:10:00 +0530

    feat(backend/rides): add fare estimation, trip_created socket emit and ride request lifecycle

--- VectraApp/backend/src/modules/ride_requests/ride-requests.module.ts
+++ VectraApp/backend/src/modules/ride_requests/ride-requests.module.ts
@@ -3,9 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RideRequestEntity } from './ride-request.entity';
 import { RideRequestsService } from './ride-requests.service';
 import { RideRequestsController } from './ride-requests.controller';
+import { TripEntity } from '../trips/trip.entity';
+import { TripRiderEntity } from '../trips/trip-rider.entity';
+import { FareModule } from '../fare/fare.module';
+import { MapsModule } from '../maps/maps.module';
+import { LocationModule } from '../location/location.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RideRequestEntity])],
+  imports: [
+    TypeOrmModule.forFeature([RideRequestEntity, TripEntity, TripRiderEntity]),
+    FareModule,
+    MapsModule,
+    LocationModule,
+  ],
   controllers: [RideRequestsController],
   providers: [RideRequestsService],
   exports: [TypeOrmModule, RideRequestsService],
